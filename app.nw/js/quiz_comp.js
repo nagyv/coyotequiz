6bbac8de48dee4e1c07b2c8f21ec0003cc9dc1bb
Viktor Nagy <viktor.nagy@gmail.com>
2016-01-24 21:21:17 +0100

    Quiz handling is ready

---
+++ app.nw/js/quiz_comp.js
@@ -0,0 +1,147 @@
+'use strict';
+
+var SummaryComponent = React.createClass({
+    displayName: 'SummaryComponent',
+
+    mixins: [Backbone.React.Component.mixin],
+    render: function () {
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'h2',
+                null,
+                'Összegzés'
+            ),
+            React.createElement(
+                'dl',
+                null,
+                React.createElement(
+                    'dt',
+                    null,
+                    'Eltöltött idő'
+                ),
+                React.createElement(
+                    'dd',
+                    null,
+                    this.props.time_spent
+                ),
+                React.createElement(
+                    'dt',
+                    null,
+                    'Kérdések száma'
+                ),
+                React.createElement(
+                    'dd',
+                    null,
+                    this.state.model.questions.length
+                ),
+                React.createElement(
+                    'dt',
+                    null,
+                    'Válaszok száma'
+                ),
+                React.createElement(
+                    'dd',
+                    null,
+                    this.props.num_answers
+                )
+            )
+        );
+    }
+});
+
+var QuizComponent = React.createClass({
+    displayName: 'QuizComponent',
+
+    mixins: [Backbone.React.Component.mixin],
+    getInitialState: function () {
+        return {
+            current: 0,
+            num_answers: 0,
+            started: Date.now(),
+            finished: false
+        };
+    },
+    getQuestion: function () {
+        return this.state.model.questions[this.state.current];
+    },
+    handleClick: function (event) {
+        if (event.target.dataset.goto == -1) {
+            let finished = Date.now();
+            this.setState({
+                num_answers: ++this.state.num_answers,
+                time_spent: finished - this.state.started,
+                finished: true
+            });
+        } else {
+            this.setState({
+                'current': event.target.dataset.goto,
+                num_answers: ++this.state.num_answers
+            });
+        }
+    },
+    createAnswer: function (answer) {
+        return React.createElement(
+            'a',
+            { className: 'col-xs-6 btn btn-default', 'data-goto': answer.goto, onClick: this.handleClick },
+            answer.text
+        );
+    },
+    createQuestion: function (question) {
+        let currentQuestion = this.getQuestion();
+        let image;
+        if (this.props.model.getImagePath(this.state.current)) {
+            let imagePath = this.props.model.getImagePath(this.state.current);
+            image = React.createElement('img', { className: 'img-responsive center-block', src: imagePath });
+        } else {
+            image = '';
+        }
+
+        return React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'div',
+                { id: 'question' },
+                React.createElement(
+                    'h2',
+                    { className: 'qtext' },
+                    currentQuestion.question
+                ),
+                image
+            ),
+            React.createElement(
+                'div',
+                { className: 'row' },
+                currentQuestion.answers.map(this.createAnswer)
+            )
+        );
+    },
+    createSummary: function () {
+        return React.createElement(SummaryComponent, { model: this.props.model, time_spent: this.state.time_spent, num_answers: this.state.num_answers });
+    },
+    render: function () {
+        let mainContent;
+        if (this.state.finished) {
+            mainContent = this.createSummary();
+        } else {
+            mainContent = this.createQuestion();
+        }
+
+        return React.createElement(
+            'div',
+            { className: 'container' },
+            React.createElement(
+                'h1',
+                null,
+                this.state.model.name
+            ),
+            mainContent
+        );
+    }
+});
+
+function renderQuiz(quiz) {
+    ReactDOM.render(React.createElement(QuizComponent, { model: quiz }), document.body);
+}
---
+++ jsx/quiz_comp.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var SummaryComponent = React.createClass({
+    mixins: [Backbone.React.Component.mixin],
+    render: function() {
+        return <div><h2>Összegzés</h2>
+            <dl>
+            <dt>Eltöltött idő</dt>
+            <dd>{this.props.time_spent}</dd>
+            <dt>Kérdések száma</dt>
+            <dd>{this.state.model.questions.length}</dd>
+            <dt>Válaszok száma</dt>
+            <dd>{this.props.num_answers}</dd>
+            </dl></div>;
+    }
+});
+
+var QuizComponent = React.createClass({
+  mixins: [Backbone.React.Component.mixin],
+  getInitialState: function() {
+    return {
+        current: 0,
+        num_answers: 0,
+        started: Date.now(),
+        finished: false
+    };
+  },
+  getQuestion: function() {
+    return this.state.model.questions[this.state.current];
+  },
+  handleClick: function(event) {
+    if(event.target.dataset.goto == -1) {
+        let finished = Date.now();
+        this.setState({
+            num_answers: ++this.state.num_answers,
+            time_spent: finished - this.state.started,
+            finished: true
+        });
+    } else {
+        this.setState({
+            'current': event.target.dataset.goto,
+            num_answers: ++this.state.num_answers
+        });
+    }
+  },
+  createAnswer: function(answer) {
+    return <a className="col-xs-6 btn btn-default" data-goto={answer.goto} onClick={this.handleClick}>{answer.text}</a>;
+  },
+  createQuestion: function(question) {
+    let currentQuestion = this.getQuestion();
+    let image;
+    if(this.props.model.getImagePath(this.state.current)) {
+        let imagePath = this.props.model.getImagePath(this.state.current);
+        image = <img className="img-responsive center-block" src={imagePath} />;
+    } else {
+        image = '';
+    }
+
+    return <div><div id="question">
+            <h2 className="qtext">{currentQuestion.question}</h2>
+            {image}
+        </div>
+        <div className="row">
+        {currentQuestion.answers.map(this.createAnswer)}
+        </div></div>;
+  },
+  createSummary: function() {
+    return <SummaryComponent model={this.props.model} time_spent={this.state.time_spent} num_answers={this.state.num_answers} />;
+  },
+  render: function () {
+    let mainContent;
+    if(this.state.finished) {
+        mainContent = this.createSummary();
+    } else {
+        mainContent = this.createQuestion();
+    }
+
+    return <div className="container">
+        <h1>{this.state.model.name}</h1>
+        {mainContent}
+        </div>;
+  }
+});
+
+function renderQuiz(quiz) {
+    ReactDOM.render(<QuizComponent model={quiz} />, document.body);
+}
